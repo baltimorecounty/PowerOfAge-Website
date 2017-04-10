4c49a5c7f3a10d15a6a6b824bf37d6a4079eb3c3
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-04-10 15:13:13 -0400

    Built out boilerplate files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp'),
+	clean = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	cssnano = require('gulp-cssnano'),
+	jshint = require('gulp-jshint'),
+	rename = require('gulp-rename'),
+	sass = require('gulp-sass'),
+	stripCode = require('gulp-strip-code'),
+	uglify = require('gulp-uglify'),
+	runSequence = require('run-sequence'),
+	stylish = require('jshint-stylish');
+
+gulp.task('clean', () => {
+	return gulp.src('dist')
+		.pipe(clean());
+});
+
+gulp.task('process-scss', () => {
+	return gulp.src('stylesheets/master.scss')
+		.pipe(sass())
+		.pipe(cssnano({ autoprefixer: false }))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('process-js', () => {
+	return gulp.src('js/**/*')
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish))
+		.pipe(stripCode({
+			start_comment: 'test-code',
+			end_comment: 'end-test-code'
+		}))
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['clean'], (callback) => {
+	return runSequence(['process-scss', 'process-js'], callback);
+});
