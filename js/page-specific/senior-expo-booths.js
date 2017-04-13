034af9baa44dad8b6c0d7a80ef3c3bf7c8900b6c
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-04-13 13:27:31 -0400

    Age persisting to DB working

--- js/page-specific/senior-expo-booths.js
+++ js/page-specific/senior-expo-booths.js
@@ -15,24 +15,42 @@ baltimoreCounty.pageSpecific.seniorExpoBooths = (($, undefined) => {
 					parentElement = snap.append(fragment),
 					booths = parentElement.selectAll('svg > g > g');	
 
-				$.each(booths, (index, snapElement) => {				
-					snapElement.click(clickedElement => {
+				$.ajax('http://localhost:1000/api/aging-expo/booth-assignments').done(boothAssigmentData => {
+					let assgnedBoothIds = [];
 
-						const $active = $(clickedElement.path[0]),
-							$target = $active.closest('g'),
-							targetId = $target.attr('id');
-						
-						$.ajax(`http://localhost:1000/api/aging-expo/booth-assignments/${targetId}`)
-							.done(() => {
-								console.log('The dishes are done, man.');
-							});
-
-
-						$target.toggleClass('highlight');
-						
+					$.each(boothAssigmentData, (index, booth) => {
+						assgnedBoothIds.push(booth.Booth_Id);
 					});
+	
+					$.each(booths, (index, snapElement) => {				
+
+						let boothAssignmentIndex = assgnedBoothIds.indexOf(snapElement.node.id * 1);
+
+						if (boothAssignmentIndex != -1) {
+							let $snapElementNode = $(snapElement.node);
+							$snapElementNode.addClass('highlight');
+						}
+
+						snapElement.click(clickedElement => {
+
+							const $active = $(clickedElement.path[0]),
+								$target = $active.closest('g'),
+								targetId = $target.attr('id');
+							
+							$.ajax(`http://localhost:1000/api/aging-expo/booth-assignments/${targetId}`)
+								.done(() => {
+									console.log(`Booth number ${targetId} has been updated.`);
+								});
+
+							$target.toggleClass('highlight');
+							
+						});
+					});
+
 				});
+
 			});
+			
 		};
 
 	return {
